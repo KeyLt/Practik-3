79faa13f56b01df3c6a0a2009857927b73442b01
Your Name <you@example.com>
2022-10-10 20:26:36 +0300

    1,5 задания сделано

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти кубы чисел от N до N
+Console.Write("Введите первое число ");
+float First_number = float.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+float Second_number = float.Parse(Console.ReadLine());
+
+for(float i = First_number; i <= Second_number; i++) 
+Console.WriteLine(Math.Pow(i,3));
+
+
+
+
---
+++ Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿// // Подсчитать сумму цифр в числе
+// using System;
+// class Program {
+// public static int Main() 
+//     {
+//     Console.Write("Введите число : ");
+//     string m = Console.ReadLine();
+//     // char [] k = m.ToCharArray();
+//     int num=int.Parse(m);
+//     int i = 0;
+//     int s = 0;
+//     while(num>0) {
+//     // i++;
+//     // num/=10;
+//         for (int j = 0; j++;)
+//         s = s + m[j];
+//         }
+// //   Console.WriteLine("Количество цифр введенного числа : {0}", i);
+//     Console.WriteLine(s);
+//     // Console.ReadKey(); // функция ожидания нажатия клавиши 
+//     return 0;
+//     }
+// }
+
+
+// // String.Length
+// // Console.Write("Введите первое число ");
+// // string First_number = Console.ReadLine();
+
+// // while(First_number>0)
+// //     for(int i, i=0, i++, i<First_number)
+
+
+// // float.Parse
+
+
+int a = int.Parse(Console.ReadLine());
+            long s = 0;
+            while (a > 0)
+            {
+                long p = a;
+                s = s + p % 10;
+                p = p /10 ;
+                
+            }
+return 0;
+ Console.WriteLine(s);
